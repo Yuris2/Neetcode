d5684d3c4a5d0ff2da10b3113c9c1ac315668702
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-04-04 08:21:52 -0400

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.py
@@ -0,0 +1,41 @@
+import collections
+class CountSquares:
+
+    def __init__(self):
+        self.plane = defaultdict(dict)
+        
+
+    def add(self, point: List[int]) -> None:
+        x,y = point
+        if y in self.plane[x]:
+            self.plane[x][y] += 1
+        else:
+            self.plane[x][y] = 1
+        
+
+    def count(self, point: List[int]) -> int:
+        qx, qy = point
+        res = 0
+
+        for dx in self.plane:
+            side1 = abs(qx - dx)
+
+            if side1 == 0:
+                continue
+            
+            for dy in self.plane[dx]:
+                side2 = abs(qy - dy)
+
+                if side1 == side2 and self.exists(qx, dy) and self.exists(dx, qy):
+                    res += self.plane[dx][dy] * self.plane[qx][dy] * self.plane[dx][qy]
+        
+        return res
+
+
+    def exists(self, x, y):
+        if x in self.plane:
+            if y in self.plane[x]:
+                return True
+        
+        return False
+        
