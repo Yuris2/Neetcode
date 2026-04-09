27fe3be05960d1979fd6456e31bbbb91e221b723
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-04-09 12:30:00 -0400

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m,n = len(matrix), len(matrix[0])
+
+        row, col = False, False
+
+        for r in range(m):
+            for c in range(n):
+                if (r == 0 or c == 0) and matrix[r][c] == 0:
+                    if r == 0:
+                        row = True
+                    if c == 0:
+                        col = True
+                elif matrix[r][c] == 0:
+                    #Set the flags (which replace double set)
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+        
+        #Dont erase the flags
+        for r in range(1,m):
+            for c in range(1,n):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        
+        if row:
+            for c in range(n):
+                matrix[0][c] = 0
+
+        if col:
+            for r in range(m):
+                matrix[r][0] = 0
+                    
+        
+        
