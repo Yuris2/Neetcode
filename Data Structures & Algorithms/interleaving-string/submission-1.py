4303b3bbc2feabd95efab432a30c77ca1eee3c9f
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-04-15 08:17:09 -0500

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n,m,p = len(s1), len(s2), len(s3)
+        cache = {}
+
+        if n + m != p:
+            return False
+        
+        def dp(i,j):
+            #If we can make it to the end
+            if i == n and j == m:
+                return True
+            if (i,j) in cache:
+                return cache[(i,j)]
+
+            c1,c2 = False, False            
+            #If a char from i == index in s3
+            if i < n and s1[i] == s3[i + j]:
+                #Advance i
+                c1 = dp(i + 1, j)
+            #Same but for s2
+            if j < m and s2[j] == s3[i + j]:
+                #Advance j
+                c2 = dp(i, j + 1)
+            
+            #See if any of these results can take us to the end
+            cache[(i,j)] = c1 or c2
+            return c1 or c2
+        
+        return dp(0,0)
+            
+            
+            
+        
