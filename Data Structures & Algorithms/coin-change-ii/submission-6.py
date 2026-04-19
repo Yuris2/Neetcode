9316f9508c28853b0217ac31dd7228d7e51833cc
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-04-19 08:54:51 -0500

    Add: coin-change-ii - submission-6

---
+++ Data Structures & Algorithms/coin-change-ii/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache = {}
+        def dp(i, total):
+            if total == amount:
+                return 1
+            if i >= len(coins) or total >= amount:
+                return 0
+            if (i, total) in cache:
+                return cache[(i,total)]
+            
+            #C1 include into total
+            c1 = dp(i, total + coins[i])
+            c2 = dp(i + 1, total)
+
+            cache[(i,total)] = c1 + c2
+            return c1 + c2
+        
+        return dp(0,0)
+        
