70c10e094114676f00b186e172baf88d832076ad
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-04-22 08:23:49 -0500

    Add: multiply-strings - submission-6

---
+++ Data Structures & Algorithms/multiply-strings/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
+        
+        res = [0] * (len(num1) + len(num2))
+
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                d1 = ord(num1[i]) - ord('0')
+                d2 = ord(num2[j]) - ord('0')
+
+                res[i + j + 1] += (d1 * d2)
+        
+        carry = 0
+        for i in range(len(res) - 1, -1, -1):
+            res[i] += carry
+            carry = res[i] // 10
+            if res[i] > 9:
+                res[i] = res[i] % 10
+        
+        while res and res[0] == 0:
+            res.pop(0)
+        
+        return "".join(str(d) for d in res)
