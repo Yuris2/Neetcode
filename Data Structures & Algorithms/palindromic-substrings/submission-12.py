4ffcea0fac976f75808f3c7fa36d662efe3d1520
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-05-11 11:31:17 -0400

    Add: palindromic-substrings - submission-12

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-12.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            res += self.isPalindrome(s,i,i)
+            res += self.isPalindrome(s,i,i+1)
+        
+        return res
+
+    
+    def isPalindrome(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                return res
+            l -= 1
+            r += 1
+            res += 1
+
+        return res
+        
