3cc00371e74fac92647937401612bcf313f89a7c
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-05-28 17:11:03 -0700

    Add: hand-of-straights - submission-8

---
+++ Data Structures & Algorithms/hand-of-straights/submission-8.py
@@ -0,0 +1,20 @@
+import collections
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = Counter(hand)
+
+        for card in sorted(hand):
+            if count[card] != 0:
+                for c in range(card, card + groupSize):
+                    if count[c] == 0:
+                        return False
+                    count[c] -= 1
+        
+        return True
+
+
+
+        
