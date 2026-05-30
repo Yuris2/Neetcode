4da325902f4e4a221b4f5706740dd210ec5ad0a1
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-05-30 10:49:55 -0700

    Add: reverse-nodes-in-k-group - submission-12

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-12.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummyNode = ListNode()
+        dummyNode.next = head
+        groupPrev = dummyNode
+
+        while True:
+            kthNode = self.findKthNode(groupPrev, k)
+
+            if not kthNode:
+                break
+            
+            groupNext = kthNode.next
+            prev, curr = groupNext, groupPrev.next
+
+            while curr != groupNext:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            
+            tmp = groupPrev.next
+            groupPrev.next = prev
+            groupPrev = tmp
+        
+        return dummyNode.next
+    
+    def findKthNode(self,head,k):
+        ptr = head
+        for _ in range(k):
+            if ptr:
+                ptr = ptr.next
+        return ptr
+    
+        
