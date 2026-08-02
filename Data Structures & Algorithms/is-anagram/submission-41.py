3899f7f66baa307ee07110502aed389f8071a7b1
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-08-02 09:00:15 -0700

    Add: is-anagram - submission-41

---
+++ Data Structures & Algorithms/is-anagram/submission-41.py
@@ -0,0 +1,13 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS = Counter(s)
+        countT = Counter(t)
+
+        return countS == countT
+        
+        
+        
