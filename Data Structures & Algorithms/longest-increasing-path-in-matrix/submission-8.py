9f05e0b94eaca61c7a07325065d9ef23e0e5c15d
Yuri Batan <55638338+Yuris2@users.noreply.github.com>
2026-08-07 15:28:26 -0700

    Add: longest-increasing-path-in-matrix - submission-8

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-8.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        R,C = len(matrix), len(matrix[0])
+        cache = {}
+
+        directions = [[1,0],[0,1],[0,-1], [-1,0]]
+
+        def isValid(r,c,prev):
+            if r < 0 or r >= R or c < 0 or c >= C:
+                return False
+            if matrix[r][c] <= prev:
+                return False
+            return True
+
+        def dp(r,c):
+            if (r,c) in cache:
+                return cache[(r,c)]
+            res = 1
+            for dr, dc in directions:
+                nR, nC = dr + r, dc + c
+                
+                if isValid(nR,nC, matrix[r][c]):
+                    res = max(res, 1 + dp(nR,nC))
+
+            cache[(r,c)] = res
+            return res
+        
+        res = 0
+        for r in range(R):
+            for c in range(C):
+                res = max(res, dp(r,c))
+
+        return res
+                
+
+                
+            
+
+        
